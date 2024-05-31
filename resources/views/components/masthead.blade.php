bc6491eb688d85287402908511342fa551cff531
Tiziano Morelli <tznmorelli@gmail.com>
2024-05-31 12:04:27 +0200

    Problemi visualizzazione

--- resources/views/components/masthead.blade.php
+++ resources/views/components/masthead.blade.php
@@ -1,7 +1,8 @@
 
-<div class="flex flex-col w-full lg:flex-row">
-    <div class="w-full m-5 flex items-center justify-center">
-        <div class="card w-full bg-base-100 shadow-xl image-full">
+<div class="flex flex-col w-full md:flex-row">
+
+    <div class="w-full my-3 flex items-center justify-center">
+        <div class="card  bg-base-100  shadow-xl image-full">
             <figure><img src="storage/img/uno.jpeg" alt="Shoes" /></figure>
             <div class="card-body">
                 <h2 class="card-title">Shoes!</h2>
@@ -12,8 +13,8 @@
             </div>
         </div>
     </div>
-    <div class="w-full m-5 flex items-center justify-center">
-        <div class="card w-full bg-base-100 shadow-xl image-full">
+    <div class="w-full my-3 flex items-center justify-center">
+        <div class="card  bg-base-100   shadow-xl image-full">
             <figure><img src="storage/img/prova.jpg" alt="Shoes" /></figure>
             <div class="card-body">
                 <h2 class="card-title">Shoes!</h2>
