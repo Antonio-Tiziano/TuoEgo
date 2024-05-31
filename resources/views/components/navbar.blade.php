f0f8bae7b275fb40a5abc798f36af49e931aa25f
Antonio <antoniominafra.dev@gmail.com>
2024-05-31 11:43:56 +0200

    aggiustato form logout

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -27,7 +27,7 @@
                 <li><a href="">Accessori</a></li>
             </ul>
         </div>
-        
+
         <div class="dropdown mx-5 dropdown-hover">
             <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
                 <a href="">Donna</a> <!-- Link to Offerte page -->
@@ -48,40 +48,30 @@
             <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                  </svg>
+                </svg>
             </div>
-            <ul tabindex="0" class="dropdown-content dropdown-left  menu p-2 shadow bg-base-100 rounded-box w-52">
+            <ul  tabindex="0" class="dropdown-content dropdown-left menu p-2 shadow bg-base-100 rounded-box w-52">
                 @if (Auth::user())
-                   
-                    <li>
-                        <form
-                        class="w-full"
-                        action="{{route('logout')}}" method="POST">
-                        @csrf
-                            <button style="width: 100%;" type="submit" class="link-neutral w-full p-5">LOGOUT</button>
-                        </form>
-                    </li>
-                 
-                    <li>
-                        <a class=" link-neutral m-1" href="{{route('home')}}">Profilo</a>
-                    </li>
-                                         
-                    @endif
-                    @guest
-                    <li><a class=" link-neutral m-3" href="{{route('login')}}">Accedi/Registrati</a></li>               
-                    @endguest
-            </ul>
-        </div>
-        {{-- <div class="btn btn-ghost btn-circle">
-            <div class="dropdown mx-5 dropdown-hover ">
-                <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-                    
-                      
-                </div>
-                <ul tabindex="0" class="dropdown-content  menu p-2 shadow bg-base-100 rounded-box w-52">
-                    
-                </ul>
-            </div>
-        </div> --}}
+
+                <li>
+                    <form
+                    id="problema"
+                    class="w-full"
+                    action="{{route('logout')}}" method="POST">
+                    @csrf
+                    <button style="width: 100%;" type="submit" class="link-neutral w-full ">Logout</button>
+                </form>
+            </li>
+
+            <li>
+                <a class=" justify-center link-neutral  m-1" href="{{route('home')}}">Profilo</a>
+            </li>
+
+            @endif
+            @guest
+            <li><a class=" justify-center link-neutral m-3" href="{{route('login')}}">Accedi/Registrati</a></li>
+            @endguest
+        </ul>
     </div>
+</div>
 </div>
