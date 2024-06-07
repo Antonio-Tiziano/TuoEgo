62ced2de8222f3b25da7324019e36637f329e721
Tiziano Morelli <tznmorelli@gmail.com>
2024-06-07 12:52:43 +0200

    Finita schermata home e footer

---
+++ resources/views/components/home.blade.php
@@ -0,0 +1,199 @@
+<div class=" justify-center mb-5 md:mb-10">
+    <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 text-center">
+        <h2 class="text-2xl font-bold tracking-tight text-gray-900">New arrivals!</h2>
+
+        <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            <div class="group relative">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                    <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men's Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                </div>
+                <div class="mt-4 flex flex-col items-start">
+                    <h3 class="text-gray-700 text-2xl font-extrabold">
+                        <a href="#">Maglia</a>
+                    </h3>
+                    <p class="mt-1 text-sm text-gray-500 pr-3">XL Park vegetable tanned leather tote</p>
+                    <h5 class="text-gray-700 mt-3 text-xl font-extrabold">
+                        <a href="#">150 €</a>
+                    </h5>
+                </div>
+            </div>
+
+            <!-- Repeat for other products... -->
+            <div class="group relative">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                    <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men's Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                </div>
+                <div class="mt-4 flex flex-col items-start">
+                    <h3 class="text-gray-700 text-2xl font-extrabold">
+                        <a href="#">Maglia</a>
+                    </h3>
+                    <p class="mt-1 text-sm text-gray-500 pr-3">XL Park vegetable tanned leather tote</p>
+                    <h5 class="text-gray-700 mt-3 text-xl font-extrabold">
+                        <a href="#">150 €</a>
+                    </h5>
+                </div>
+            </div>
+
+            <div class="group relative">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                    <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men's Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                </div>
+                <div class="mt-4 flex flex-col items-start">
+                    <h3 class="text-gray-700 text-2xl font-extrabold">
+                        <a href="#">Maglia</a>
+                    </h3>
+                    <p class="mt-1 text-sm text-gray-500 pr-3">XL Park vegetable tanned leather tote</p>
+                    <h5 class="text-gray-700 mt-3 text-xl font-extrabold">
+                        <a href="#">150 €</a>
+                    </h5>
+                </div>
+            </div>
+
+            <div class="group relative">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                    <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men's Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                </div>
+                <div class="mt-4 flex flex-col items-start">
+                    <h3 class="text-gray-700 text-2xl font-extrabold">
+                        <a href="#">Maglia</a>
+                    </h3>
+                    <p class="mt-1 p text-sm text-gray-500 pr-3">XL Park vegetable tanned leather tote</p>
+                    <h5 class="text-gray-700 mt-3 text-xl font-extrabold">
+                        <a href="#">150 €</a>
+                    </h5>
+                </div>
+            </div>
+
+            <!-- More products... -->
+        </div>
+        
+    </div>
+    <div class="mx-auto text-center">
+        <button class="btn btn-active btn-neutral">Scopri di più</button>
+    </div>    
+</div>
+
+
+
+<div class="container mx-auto">
+    <div class="flex flex-col md:flex-row justify-center">
+
+        <div class="w-full lg:w-1/4 md:w-1/3 p-5 flex items-center justify-center m-4">
+            <!-- Card 1 -->
+            <div class="card bg-base-100 shadow-xl image-full relative">
+                <figure><img src="storage/img/uno.jpeg" alt="Shoes" /></figure>
+                <div class="card-body absolute bottom-0 left-0 right-0 flex flex-col items-center justify-center text-center p-4">
+                    <h2 class="card-title font-bold text-5xl px-3 pt-5">Sail into Summer!</h2>
+                    <p>With the latest arrivals</p>
+                    <div class="card-actions m-2">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="w-full lg:w-1/4 md:w-1/3 p-5 flex items-center justify-center m-4">
+            <!-- Card 2 -->
+            <div class="card bg-base-100 shadow-xl image-full relative">
+                <figure><img src="storage/img/uno.jpeg" alt="Shoes" /></figure>
+                <div class="card-body absolute bottom-0 left-0 right-0 flex flex-col items-center justify-center text-center p-4">
+                    <h2 class="card-title font-bold text-5xl px-3 pt-5">Sail into Summer!</h2>
+                    <p>With the latest arrivals</p>
+                    <div class="card-actions m-2">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="w-full lg:w-1/4 md:w-1/3 p-5 flex items-center justify-center m-4">
+            <!-- Card 3 -->
+            <div class="card bg-base-100 shadow-xl image-full relative">
+                <figure><img src="storage/img/IMG_4195.jpg" alt="Shoes" /></figure>
+                <div class="card-body absolute bottom-0 left-0 right-0 flex flex-col items-center justify-center text-center p-4">
+                    <h2 class="card-title font-bold text-5xl px-3 pt-5">Sail into Summer!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions m-2">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+  
+    </div>
+</div>
+
+<div class=" justify-center mb-5 md:mb-10">
+    <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 text-center">
+        <h2 class="text-2xl font-bold tracking-tight text-gray-900">New arrivals!</h2>
+
+        <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            <div class="group relative">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                    <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men's Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                </div>
+                <div class="mt-4 flex flex-col items-start">
+                    <h3 class="text-gray-700 text-2xl font-extrabold">
+                        <a href="#">Maglia</a>
+                    </h3>
+                    <p class="mt-1 text-sm text-gray-500 pr-3">XL Park vegetable tanned leather tote</p>
+                    <h5 class="text-gray-700 mt-3 text-xl font-extrabold">
+                        <a href="#">150 €</a>
+                    </h5>
+                </div>
+            </div>
+
+            <!-- Repeat for other products... -->
+            <div class="group relative">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                    <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men's Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                </div>
+                <div class="mt-4 flex flex-col items-start">
+                    <h3 class="text-gray-700 text-2xl font-extrabold">
+                        <a href="#">Maglia</a>
+                    </h3>
+                    <p class="mt-1 text-sm text-gray-500 pr-3">XL Park vegetable tanned leather tote</p>
+                    <h5 class="text-gray-700 mt-3 text-xl font-extrabold">
+                        <a href="#">150 €</a>
+                    </h5>
+                </div>
+            </div>
+
+            <div class="group relative">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                    <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men's Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                </div>
+                <div class="mt-4 flex flex-col items-start">
+                    <h3 class="text-gray-700 text-2xl font-extrabold">
+                        <a href="#">Maglia</a>
+                    </h3>
+                    <p class="mt-1 text-sm text-gray-500 pr-3">XL Park vegetable tanned leather tote</p>
+                    <h5 class="text-gray-700 mt-3 text-xl font-extrabold">
+                        <a href="#">150 €</a>
+                    </h5>
+                </div>
+            </div>
+
+            <div class="group relative">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                    <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men's Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                </div>
+                <div class="mt-4 flex flex-col items-start">
+                    <h3 class="text-gray-700 text-2xl font-extrabold">
+                        <a href="#">Maglia</a>
+                    </h3>
+                    <p class="mt-1 p text-sm text-gray-500 pr-3">XL Park vegetable tanned leather tote</p>
+                    <h5 class="text-gray-700 mt-3 text-xl font-extrabold">
+                        <a href="#">150 €</a>
+                    </h5>
+                </div>
+            </div>
+
+            <!-- More products... -->
+        </div>
+        
+    </div>
+    <div class="mx-auto text-center">
+        <button class="btn btn-active btn-neutral">Scopri di più</button>
+    </div>    
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,5 @@
 <x-layout>
     <x-masthead></x-masthead>
+    <x-home></x-home>
+    <x-footer></x-footer>
 </x-layout>
